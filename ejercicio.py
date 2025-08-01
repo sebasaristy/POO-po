55913ef1ee250bc4371fb296c3ab57616d5d96e1
Chirri <sebasaristy.0809@gmail.com>
2025-08-01 11:17:14 -0500

    no hice cambios

--- ejercicio.py
+++ ejercicio.py
@@ -17,13 +17,33 @@ class controlAcademico:
         print("Nota 2: ", self.notaDos)
         print("Nota 3: ", self.notaTres)
 
-nombreEstudiante = input("Ingrese nombre estudiante: ")
-edadEstudiante = int(input("Ingrese la edad del estudiante: "))
-notaUnoEstudiante = float(input("Ingrese la nota 1: "))
-notaDosEstudiante = float(input("Ingrese la nota 2: "))
-notaTresEstudiante = float(input("Ingrese la nota 3: "))
+    promedioEstudiante = promedio
 
-estudiante = controlAcademico(nombreEstudiante, edadEstudiante, notaUnoEstudiante, notaDosEstudiante, notaTresEstudiante)
-promedioEstudiante = estudiante.promedio()
+print("Sistema de gestion de estudiantes")
+listaEstudiantes = []
+while True: 
+    print("\nSeleccione la opcion que desea:")
+    print("1. Agregar estudiante")
+    print("2. Mostrar informacion de estudiantes")
+    print("0. Salir")
+    opcion = int(input("Ingrese el numero: "))
 
-print("El promedio de ", estudiante.nombre, "es de: ", promedioEstudiante)
+    if opcion == 1:
+        nombreEstudiante = input("Ingrese nombre estudiante: ")
+        edadEstudiante = int(input("Ingrese la edad del estudiante: "))
+        notaUnoEstudiante = float(input("Ingrese la nota 1: "))
+        notaDosEstudiante = float(input("Ingrese la nota 2: "))
+        notaTresEstudiante = float(input("Ingrese la nota 3: "))
+        estudianteNuevo = controlAcademico(nombreEstudiante, edadEstudiante, notaUnoEstudiante, notaDosEstudiante, notaTresEstudiante)    
+        listaEstudiantes.append(estudianteNuevo)
+    
+    if opcion == 2:
+        numeroEstudiantes = len(listaEstudiantes)
+        print(f'\nEl numero de Estudiantes es de {numeroEstudiantes}')
+        for estudiante in listaEstudiantes:
+            print("El nombre del Estudiante es: ", estudiante.nombre)
+            print("El promedio del estudiante es: ", estudiante.promedio())
+
+    if opcion == 0:
+        print("Gracias")
+        break
