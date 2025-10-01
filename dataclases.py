b336c6dd3a5a1d8f385c4543a997935789022447
Chirri <sebasaristy.0809@gmail.com>
2025-10-01 11:18:12 -0500

    mico

---
+++ dataclases.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass, field, asdict
+from typing import List
+
+
+@dataclass
+class Persona:
+    nombre: str
+    edad: int = field(default=36)
+
+    def retornar_edad_por_2(self) -> int:
+        return self.edad * 2
+     
+persona1 = Persona("Juan")
+print(asdict(persona1))
+
+
+@dataclass
+class Puesto:
+    nombre: str
+    persona: Persona
+
+puesto1 = Puesto("Desarrolador", persona1)
+print(asdict(puesto1))
+
+print(persona1.retornar_edad_por_2())
+
+
+@dataclass
+class Grupo:
+    personas: List[Persona] = field(default_factory=list)
+
+
+grupo1 = Grupo()
+grupo1.personas.append(persona1)
+print(grupo1.personas)
